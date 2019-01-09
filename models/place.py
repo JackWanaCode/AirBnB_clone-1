2b1d23d0887ba18dce0241650a5aca335731efb5
JackWanaCode <kazucuong@yahoo.com>
2019-01-09 00:25:03 +0000

    import models

--- models/place.py
+++ models/place.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This is the place class"""
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.orm import relationship, backref
