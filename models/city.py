d0251169f35755630a9653a9c9c5e8aa3f86e058
JackWanaCode <kazucuong@yahoo.com>
2019-01-09 00:09:49 +0000

    change nullable true to false

--- models/city.py
+++ models/city.py
@@ -17,6 +17,5 @@ class City(BaseModel, Base):
     state_id = Column(String(60), ForeignKey
                       ('states.id', ondelete="CASCADE"),
                       nullable=False)
-
     places = relationship("Place", cascade='all, delete-orphan',
                           backref='cities')
