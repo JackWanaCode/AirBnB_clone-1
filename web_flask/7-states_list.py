fec303eebd5d92ae774a353ed53dd197b8349504
JackWanaCode <kazucuong@yahoo.com>
2019-01-31 06:38:16 +0000

    first commit

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+dict_of_state = {}
+all_states = storage.all("State")
+for obj in all_states.values():
+    dict_of_state[obj.__dict__['name']] = obj.__dict__['id']
+list_of_state = list(sorted(dict_of_state.items()))
+
+
+@app.route('/states_list')
+@app.route('/states_list/<list_of_state>')
+def print_state_list():
+    """return a html content"""
+    return render_template('7-states_list.html', list_of_state=list_of_state)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
