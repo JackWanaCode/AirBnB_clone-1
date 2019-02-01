15e40369043925a32a490a6d7e00af36d6c2e6e7
JackWanaCode <kazucuong@yahoo.com>
2019-02-01 20:17:00 +0000

    corret fetching all data of state and city

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,35 +10,12 @@ from os import getenv
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-list_of_state = []
-list_of_city = []
-dic = {}
-dict_of_state = {}
-#all_states = storage.all(State)
-
+all_states = None
 
 if getenv("HBNB_TYPE_STORAGE") == 'db':
     all_states = storage.all("State")
-    for st in (storage._DBStorage__session.query(State).
-               order_by(State.name).all()):
-        list_of_state.append([st.id, st.name])
-        for ct in (storage._DBStorage__session.query(City).order_by(City.name).
-                   filter(City.state_id == st.id).all()):
-            list_of_city.append(ct)
-        dic[st.name] = list_of_city
-        list_of_city = []
 else:
     all_states = storage.all(State)
-    for obj in all_states.values():
-        dict_of_state[obj.__dict__['name']] = obj.__dict__['id']
-        dic[obj.__dict__['id']] = obj.cities
-    list_of_state = list(sorted(dict_of_state.items()))
-
-#print(list_of_state)
-for k, v in dic.items():
-#    print(type(v[0]))
-    for item in dic[k]:
-        print(item.name, item.id)
 
 
 @app.route('/cities_by_states')
@@ -46,7 +23,7 @@ def print_state_list():
 
     """return a html content"""
     return render_template('8-cities_by_states.html',
-                           list_of_state=list_of_state, dic=dic)
+                           all_states=all_states)
 
 
 @app.teardown_appcontext
